0013e499c6058bba65c7b3d85d7f207fca050a6f
Harish Saud <saudharish770@gmail.com>
2025-02-20 23:06:46 +0545

    Update hidden.py

--- Python_basic_python/hidden.py
+++ Python_basic_python/hidden.py
@@ -1,12 +1,20 @@
-# Hide the message in a file
+# Hide the message in a file with logs:
+import datetime
+import requests
+
+# Get current time and public IPv4 address
+time_stamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+ip_address = requests.get('https://api.ipify.org').text
+
+# Save secret message
 secret = input("Enter a secret message: ")
 with open("hidden.txt", "w") as file:
-    file.write(secret)
-print("✅ Message hidden successfully!")
+    file.write(f"Time: {time_stamp}\nIP: {ip_address}\nSecret: {secret}\n")
 
-# Ask if the user wants to reveal it
-if input("Do you want to reveal the message? (yes/no): ").lower() == "yes":
-    with open("hidden.txt", "r") as file:
-        print("🔍 Hidden message:", file.read())
+print("✅ Message hidden!")
+
+# Reveal message
+if input("Reveal message? (yes/no): ").lower() == "yes":
+    print(open("hidden.txt").read())
 else:
     print("🔒 Message remains hidden.")
